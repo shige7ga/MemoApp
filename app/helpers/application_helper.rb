ae78d6bc00909d4ba112856f74a6281ffd8ca6ff
Shigeyuki Nagai <ameft80s@gmail.com>
2019-06-06 06:42:36 +0000

    動的に変化するタイトルの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # 各ページごとで適したタイトルを返すメソッド
+  def full_title(page_title = '')
+    base_title = "MemoApp"
+    # ページのタイトルが空の場合
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
